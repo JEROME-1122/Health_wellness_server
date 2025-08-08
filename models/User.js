d36bfa9e34878b6bc9043b488f2fdc94201aefbd
Jerome Franklin X <118640107+JEROME-1122@users.noreply.github.com>
2025-08-08 23:48:28 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,8 +4,14 @@ const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
   password: String,
-  // resetPasswordToken: String,
-  // resetPasswordExpires: Date
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+  resetOtp: { type: Number, default: 0 },
+  resetOtpExpireAt: {
+    type: Number,
+    default: 0,
+  },
 });
 
 export default mongoose.model('User', UserSchema);
+
